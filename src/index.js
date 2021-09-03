b3bc199df55eb5bb9ef6d48d860a6e994f92af07
2787363534 <2787363534@qq.com>
2021-09-03 11:18:54 +0800

    实现useState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// 让m和n不相互影响
+let _state = []
+let index = 0
+const myUseState = initialValue => {
+  // 只能将index的值保存下来，对currentIndex进行操作，不然无法让index加一
+  const currentIndex = index
+  _state[currentIndex] = _state[currentIndex] === undefined ? initialValue : _state[currentIndex]
+  const setState = newValue => {
+    _state[currentIndex] = newValue
+    render()
+  }
+  index +=1
+  return [_state[currentIndex], setState]
+}
+const render = () => {
+  // 在渲染组件之前将index的值重置，否则会一直往上加
+  index = 0
+  ReactDOM.render( < App / > , rootElement)
+}
+
+function App() {
+  const [n,setN] = React.useState(0)
+  const [m,setM] = React.useState(0)
+  return(
+    <div className="App">
+      <p>{n}</p>
+      <p>
+        <button onClick={()=>setN(n+1)}>+1</button>
+      </p>
+      <p>{m}</p>
+      <p>
+        <button onClick={()=>setM(m+1)}>+1</button>
+      </p>
+    </div>
+  )
+}
+ReactDOM.render( < App / > , rootElement)
